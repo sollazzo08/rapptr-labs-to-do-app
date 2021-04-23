99b0935c56ab7aacf3177045f860a4fe083630b2
sollazzo <msollazzo08@gmail.com>
2021-04-23 11:57:55 -0400

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './styles/App.css';
 import LoginForm from './components/LoginForm'
+import ToDoList from './components/ToDoList'
+
+import {Route, Switch } from 'react-router-dom'
 function App() {
   return (
+    <Switch>
+      <Route path="/toDoList" component={ToDoList} />
+      <Route exact="/" component={LoginForm}/>
     <div className="App">
      <LoginForm />
     </div>
+
+    </Switch>
   );
 }
 
---
+++ src/components/ToDoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ToDoList = () => {
+  return (  
+    <h1>To-Do-List</h1>
+  );
+}
+ 
+export default ToDoList;
